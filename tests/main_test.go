19a5540cee683d64ce8be3781ac39e38779f024d
Will May <will.j.may@gmail.com>
2022-08-30 09:22:14 +0100

    Make it faster to start a new shell
    Change various scripts to avoid running commands as part of starting a
    new shell, such as `brew --prefix`, as the outputs of these commands
    don't change frequently enough. Also make sure the zcompdump file is
    only replaced when needed - probably when FPATH is changed.
    
    Also revert the change to the tests to make the macOS compatible as they
    were broken in Docker.

--- tests/main_test.go
+++ tests/main_test.go
@@ -7,6 +7,7 @@ import (
 	"testing"
 
 	"github.com/gruntwork-io/terratest/modules/shell"
+	"github.com/samber/lo"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
@@ -86,8 +87,25 @@ func findTool(t *testing.T, tool string) string {
 }
 
 func getDefaultShell(t *testing.T) string {
-	userShell, present := os.LookupEnv("SHELL")
-	require.True(t, present, "missing SHELL env")
+	user := shell.RunCommandAndGetOutput(t, shell.Command{
+		Command: "whoami",
+	})
 
-	return userShell
+	content, err := os.ReadFile("/etc/passwd")
+	require.NoError(t, err)
+
+	lines := strings.Split(string(content), "\n")
+
+	userShells := lo.FilterMap(lines, func(line string, _ int) (string, bool) {
+		parts := strings.Split(line, ":")
+		if parts[0] != user {
+			return "", false
+		}
+
+		return parts[len(parts)-1], true
+	})
+
+	assert.Len(t, userShells, 1)
+
+	return userShells[0]
 }
